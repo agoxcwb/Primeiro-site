61f2c94c17147c3bacf3eb535ca576287d76d627
agoxcwb <thyagofeprestes@gmail.com>
2024-10-20 23:49:00 -0300

    Primeiro Site

---
+++ script.js
@@ -0,0 +1,12 @@
+document.body.style.overflow = 'hidden';
+document.body.style.position = 'fixed';
+document.documentElement.style.margin = '0';
+document.documentElement.style.padding = '0';
+document.documentElement.style.width = '100%';
+document.documentElement.style.height = '100%';
+
+document.body.style.margin = '0';
+document.body.style.padding = '0';
+document.body.style.width = '100%';
+document.body.style.height = '100%';
+document.body.style.overflow = 'hidden';
